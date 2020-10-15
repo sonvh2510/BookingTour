a8ad0671b260fe4edfc84c9596f1de0f07b328ca
Den <den@Macbooks-MacBook-Pro.local>
2020-10-15 15:34:59 +0700

    about

--- src/js/main.js
+++ src/js/main.js
@@ -15,7 +15,7 @@ function swiperSlider(){
 		},
 		spaceBetween: 20,
 		speed: 500,
-		loop: true,
+		// loop: true,
 		navigation: {
 			nextEl: '.ab-2 .swiper-next',
 			prevEl: '.ab-2 .swiper-prev',
@@ -40,4 +40,90 @@ function swiperSlider(){
 			},
 		}
 	});
+	let imgGallery = new Swiper('.slide-image .swiper-container', {
+		autoplay: {
+			delay: 4500,
+		},
+		spaceBetween: 20,
+		speed: 500,
+		// loop: true,
+		navigation: {
+			nextEl: '.slide-image .swiper-next',
+			prevEl: '.slide-image .swiper-prev',
+		},
+		pagination: {
+			el: '.slide-image .swiper-pagination',
+			type: 'bullets',
+			clickable: true,
+		},
+		breakpoints: {
+			320: {
+				slidesPerView: 1,
+			},
+			576: {
+				slidesPerView: 2,
+			},
+			768: {
+				slidesPerView: 3,
+			},
+			1025: {
+				slidesPerView: 4,
+			},
+		}
+	});
+	let raiting = new Swiper('.slide-raiting .swiper-container', {
+		autoplay: {
+			delay: 4500,
+		},
+		spaceBetween: 20,
+		speed: 500,
+		centeredSlides: true,
+		loop: true,
+		navigation: {
+			nextEl: '.slide-raiting .swiper-next',
+			prevEl: '.slide-raiting .swiper-prev',
+		},
+		pagination: {
+			el: '.slide-raiting .swiper-pagination',
+			type: 'bullets',
+			clickable: true,
+		},
+		breakpoints: {
+			320: {
+				slidesPerView: 1,
+			},
+			576: {
+				slidesPerView: 2,
+			},
+			768: {
+				slidesPerView: 3,
+			},
+		}
+	});
+	//about-slide-year
+	let galleryThumbs = new Swiper('.slide-year .gallery-thumbs', {
+		spaceBetween: 10,
+		slidesPerView: 9,
+		// loop: true,
+		freeMode: true,
+		loopedSlides: 5, //looped slides should be the same
+		watchSlidesVisibility: true,
+		watchSlidesProgress: true,
+	  });
+	  let galleryTop = new Swiper('.gallery-top', {
+		spaceBetween: 10,
+		loop: true,
+		loopedSlides: 5, //looped slides should be the same
+		navigation: {
+		  nextEl: '.swiper-button-next',
+		  prevEl: '.swiper-button-prev',
+		},
+		thumbs: {
+		  swiper: galleryThumbs,
+		},
+	  });
+}
+
+funtion question(){
+	
 }
